1d47e60f63cf7830c3a9e2bdee165fc4cacd9f83
apple <apple@MacBook-Air-apple.local>
2020-04-22 16:31:55 +0300

    "init"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'free-world';
+
+  ngOnInit() {
+    debugger;
+
+    let mainModal = document.querySelector('#index-modal-container') as HTMLElement;
+
+    mainModal.className = "main-content";
+    // $('.button').click(function(){
+    //   var buttonId = $(this).attr('id');
+    //   $('#modal-container').removeAttr('class').addClass(buttonId);
+    //   $('body').addClass('modal-active');
+
+    // $('#modal-container').click(function(){
+    //   $(this).addClass('out');
+    //   $('body').removeClass('modal-active');
+    // });
+
+  }
 }
+
